084eaf630e879e5c9acead1837f4b24f3c5b20fb
SreeparnaSingha <sreeparnasingha0237@gmail.com>
2023-05-22 20:10:15 +0530

    Approved

--- src/pages/ApproveDoctor.jsx
+++ src/pages/ApproveDoctor.jsx
@@ -122,8 +122,8 @@ const ApproveDoctor = () => {
                   className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 >
                   <option value="">Select an option</option>
-                  <option value="approved">Approved</option>
-                  <option value="pending">Pending</option>
+                  <option value="Approved">Approved</option>
+                  <option value="Pending">Pending</option>
                 </select>
               </div>
 
